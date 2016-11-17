ecd6c7cf92926439650f0afadd3cac14cfdd8afc
eerotpt <eero.sakson@tptlive.ee>
2016-11-17 14:47:25 +0200

    14:47

---
+++ src/Exercise_7_3.java
@@ -0,0 +1,18 @@
+/**
+ * Created by opilane on 17.11.2016.
+ */
+public class Exercise_7_3 {
+
+    public static void power(double a,int b){
+        double result=a;
+        while (b>1){
+            b=b-1;
+            result=result*a;
+        }
+        System.out.println("result = "+result);
+    }
+
+    public static void main(String[] args){
+        power(3,4);
+    }
+}
