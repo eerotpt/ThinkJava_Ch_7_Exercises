0388dadcfde3d2cadf6f613f0dd958093607233e
eerotpt <eero.sakson@tptlive.ee>
2016-11-17 19:48:33 +0200

    19:

--- src/Exercise_7_3.java
+++ src/Exercise_7_3.java
@@ -2,7 +2,7 @@
  * Created by opilane on 17.11.2016.
  */
 public class Exercise_7_3 {
-
+//computing number "a" in power "b" (a raised to the power b)
     public static void power(double a,int b){
         double result=a;
         while (b>1){
