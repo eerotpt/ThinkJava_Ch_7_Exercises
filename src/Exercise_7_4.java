728f7c6d3dc72e6b88002971857117eff8c0fd60
eerotpt <eero.sakson@tptlive.ee>
2016-11-17 19:30:36 +0200

    19:30

--- src/Exercise_7_4.java
+++ src/Exercise_7_4.java
@@ -2,4 +2,17 @@
  * Created by opilane on 17.11.2016.
  */
 public class Exercise_7_4 {
+
+    public static void factorial(int n){
+        int c=n;
+        int d=n-1;
+        while (d>0){
+            c=c*d;
+            d=d-1;
+        } System.out.print(n+"!="+c);
+    }
+
+    public static void main(String[] args){
+        factorial(5);
+    }
 }
